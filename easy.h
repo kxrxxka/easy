0ad487144c773f1c3c9098048d86b493f1cb4d29
kxrxxka <145845389+kxrxxka@users.noreply.github.com>
2024-03-04 14:18:45 +0300

    Add files via upload

---
+++ easy.h
@@ -0,0 +1,9 @@
+#ifndef EASY_H_INCLUDED
+#define EASY_H_INCLUDED
+void itc_name();
+void itc_fio();
+int itc_abs(int num);
+double itc_fabs(double num);
+int itc_revnbr(int num);
+
+#endif // EASY_H_INCLUDED
